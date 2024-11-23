ea44a844d401836d4224d1a7e2c8a72236bcd14e
alexander.rodriguez <alexander.rodriguez@enmedio.co>
2024-11-23 09:22:26 -0500

    [feature][context custom | general colors | welcome | types | routes]

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,25 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Welcome from '../pages/Welcome';
+
+
+export default function Routes() {
+  const Nav = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      {/* Inicia la navegacion dependiendo del estado de login */}
+        <Nav.Navigator initialRouteName={"Welcome"}>
+            <Nav.Screen
+            name="Welcome"
+            component={Welcome}
+            options={{
+              title: 'Login',
+              headerShown: false,
+              gestureEnabled: false,
+            }}></Nav.Screen>
+        </Nav.Navigator>
+    </NavigationContainer>
+  )
+}
